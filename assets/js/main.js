8b29a2245be28af56192e0dfed848abca96f89e6
NadeemAsh <dev.nad.ash@gmail.com>
2022-03-19 18:01:58 +0530

    Header Styling Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 // Grab elements
 
+// Custom Function to select elements without breaking from the DOM
+const selectElement = selector => {
+    const element = document.querySelector(selector);
+    if (element) return element;
+    throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly`)
+}
+
 //Nav styles on scroll
 
 // Open menu & search pop-up
