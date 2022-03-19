a06456950cef6bc0762cd03cfd67f5ba02a124ba
NadeemAsh <dev.nad.ash@gmail.com>
2022-03-19 18:14:37 +0530

    Make theme switching persistent across refresh

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 // Grab elements
-
 // Custom Function to select elements without breaking from the DOM
 const selectElement = selector => {
     const element = document.querySelector(selector);
@@ -8,13 +7,49 @@ const selectElement = selector => {
 }
 
 //Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
 
+    // Add Header Box Shadow when scrolled down
+    if (this.scrollY >= 15) {
+        headerElement.classList.add('activated');
+    } else {
+        headerElement.classList.remove('activated');
+    }
+};
+window.addEventListener('scroll', scrollHeader);
 // Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
 
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
 
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if (currentTheme) {
+    bodyElement.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('light-theme');
+
+    if (bodyElement.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'theme-active');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
+});
 
 // Swiper
